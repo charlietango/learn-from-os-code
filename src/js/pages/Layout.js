3b492ed46ce2bd97503c7e5b569e2d5ce7676e6b
Catalin Tudorache <charlietango592>
2017-01-17 20:21:10 +0200

    Enhanced the visual aspect of the app. Replaced css with scss, which is modularized.

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import Header from "../components/layout/Header";
 
+import style from "../../styles/main.scss";
+
 export default class Layout extends React.Component {
   render() {
     return(
