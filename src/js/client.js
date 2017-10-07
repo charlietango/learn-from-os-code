904c0eef565797aef7ff327aad1356910ddf11dd
Catalin Tudorache <tudorache.catalin47@gmail.com>
2017-10-07 13:35:50 -0500

    Fixed browser history error

--- src/js/client.js
+++ src/js/client.js
@@ -1,7 +1,7 @@
-import React from "react";
+  import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux"
-import { Router, Route, IndexRoute, hashHistory } from "react-router";
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
 
 import About from "./pages/About";
 import Layout from "./pages/Layout";
@@ -13,7 +13,7 @@ const app = document.getElementById('app');
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={hashHistory}>
+    <Router history={browserHistory}>
       <Route path="/" component={Layout}>
         <IndexRoute component={Main}></IndexRoute>
         <Route path="about" name="about" component={About}></Route>
